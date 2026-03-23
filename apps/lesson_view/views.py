80d76498eafed76b7083d60d3f1664a91fa05f52
Murakasi <murakasi.ruslan@gmail.com>
2026-03-23 09:53:27 +0100

    fixe smole bag in path on views.py

--- apps/lesson_view/views.py
+++ apps/lesson_view/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpRequest, HttpResponse
 def main(request: HttpRequest) -> HttpResponse:
-    return render(request, 'base.html' , {'name': 'Ruslan'})
+    return render(request, 'lesson_view/lesson1.html' , {'name': 'Ruslan'})
