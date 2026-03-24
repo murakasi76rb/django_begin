7a1f9bad8369495e0a4aa82b137e9523f3a5a148
Murakasi <murakasi.ruslan@gmail.com>
2026-03-24 07:21:16 +0100

    restore models.py form Render site  pg_dunp

--- apps/lesson_view/models.py
+++ apps/lesson_view/models.py
@@ -1,3 +1,25 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+class Cuisine(models.TextChoices):
+    ITALY = 'italy', 'ITALY'
+    FRENCH = 'french', 'French'
+    UKRAINE = 'ukraine', 'Ukraine'
+
+class Menu(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(unique=True, max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateField()
+    updated_at = models.DateTimeField()
+    couisine = models.CharField(max_length=20, choices=Cuisine, default=Cuisine.UKRAINE)
+
+    class Meta:
+        managed = False
+        db_table = 'menu'
